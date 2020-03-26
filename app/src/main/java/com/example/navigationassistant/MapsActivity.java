c481cc004898f63dafb7a407958875cb178b3eec
dtaneli16 <dtaneli16@ku.edu.tr>
2020-03-26 17:24:32 +0300

    add camera animation to istanbul

--- app/src/main/java/com/example/navigationassistant/MapsActivity.java
+++ app/src/main/java/com/example/navigationassistant/MapsActivity.java
@@ -39,9 +39,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng istanbul = new LatLng(41, 29);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(istanbul));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(istanbul, 10));
     }
 }
